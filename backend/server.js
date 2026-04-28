982d133e3ceb8a54f7f30e839a802a97d866ce10
Bryan Arroyo Carrillo <barroyo@Bryans-MacBook-Pro.local>
2026-04-28 16:54:47 -0600

    Initial REST API for goals

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const colors = require('colors')
+const { connectDB } = require('./config/db')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+const app = express()
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use('/api/goals', require('./routes/goalsRoutes'))
+app.use(errorHandler)
+
+app.listen(port, () => console.log('running on port http://localhost:8080'))
